43e132421a4a31bf761d265fcca03fe6cd250e80
Rackel <odiwuorrackel@gmail.com>
2024-07-11 17:18:13 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -7,7 +7,7 @@ if (selectedRadioButton) {
    const userAnswer = selectedRadioButton.value
  const correctAnswer = "4"
  if (userAnswer === correctAnswer){
-   document.getElementById("feedback").textContent = "Correct!Well done.";
+   document.getElementById("feedback").textContent = "Correct! Well done.";
 
  }else{
    document.getElementById("feedback").textContent = "That's incorrect.Try again!";
